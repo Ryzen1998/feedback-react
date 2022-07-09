e00c3234fb7cf30a5194f1f74e7a1ab15af5ab3f
Ryzen1998 <apexashwin@gmail.com>
2022-07-09 11:21:15 +0300

    added text validation

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,45 @@
+import {React,useState} from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
+
+function FeedbackForm() {
+    const [text,setText]=useState('')
+    const [btnDisabled,setBtnDisabled]=useState(true)
+    const [message,setMessage]=useState('')
+   
+    const handleTextChange =(e)=>{
+      if(text==='' ){
+        setBtnDisabled(true)
+        setMessage(null)
+      }
+      else if(text!==''&& text.trim().length<10){
+        setMessage('Text must be atleast 10 characters')
+        setBtnDisabled(true)
+
+      }
+      else
+      {
+        setMessage(null)
+        setBtnDisabled(false)
+      }
+
+       setText(e.target.value);
+
+    }
+  return (
+    <Card>
+        <form>
+        <h2>How would you rate your experience?</h2>
+        <div className='input-group'>
+            <input onChange={handleTextChange} type='text' placeholder='write your review' value={text}/>
+            <Button type='submit' isDisabled={btnDisabled} >send</Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+        </form>
+    
+    </Card>
+  )
+}
+
+export default FeedbackForm
